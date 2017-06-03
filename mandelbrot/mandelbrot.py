1cee9d1aa3d48a3b73b8d3b777b0f4165971f51f
tblaha <till.blaha@web.de>
2017-06-03 15:37:47 +0200

    added data files

--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -21,11 +21,14 @@ while True:
         print "This was not a number or the number was out of range, try again!"
 
 data = {'niter':50.,'radius':2.,'xmin':-2.,'xmax':1.,'ymin':-1.5,'ymax':1.5,'npixels':500.}
-file = genfromtxt(path+choice,dtype="string",delimiter=",")
+file = genfromtxt(path+choice,dtype="string",delimiter=";")
 for line in file:
     if line[-1]:
         data[line[0]]=float(line[-1])
 
+
+
+#***********************************#
 c = ones((int(data['npixels']),int(data['npixels'])),dtype=complex)
 
 A = arange(data['xmin'],data['xmax'],(data['xmax']-data['xmin'])/(data['npixels']))
@@ -44,5 +47,5 @@ def iterate(c, z = 0 + 0j, i = 0):
     i +=1
     return iterate(c, where(abs(zplus1) < data['radius']*1.01, zplus1, data['radius']) ,i)
 
-plt.imshow(abs(iterate(c)),plt.get_cmap('Blues'))
+plt.imshow(abs(iterate(c)),plt.get_cmap('seismic'))
 plt.show()
